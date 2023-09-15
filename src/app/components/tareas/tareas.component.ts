7bec196806d095444334c208d37b2eb635359e9f
MiguelPasaje <cartel-dy@hotmail.com>
2023-09-14 22:33:50 -0500

    local storage

--- src/app/components/tareas/tareas.component.ts
+++ src/app/components/tareas/tareas.component.ts
@@ -10,7 +10,14 @@ export class TareasComponent implements OnInit {
   listTareas:Tarea[] = []
   nombreTarea = ''
 
+
+
   ngOnInit(): void {
+
+    const listaGuardada = localStorage.getItem('listaTareas');
+    if (listaGuardada) {
+      this.listTareas = JSON.parse(listaGuardada);
+    }
 
 
   }
@@ -20,15 +27,19 @@ export class TareasComponent implements OnInit {
     const tarea = new Tarea(this.nombreTarea,false)
     this.listTareas.push(tarea)
     this.nombreTarea = ''
+    localStorage.setItem('listaTareas', JSON.stringify(this.listTareas));
   }
 
   eliminarTarea(indexTarea:number):void{
     this.listTareas = this.listTareas.filter((tarea, indice)=> indice !== indexTarea)
+
+    localStorage.setItem('listaTareas', JSON.stringify(this.listTareas));
   }
 
   actualizarTarea(tarea:Tarea,indexTarea:number){
     this.listTareas[indexTarea].estado = !tarea.estado
 
+    localStorage.setItem('listaTareas', JSON.stringify(this.listTareas));
   }
 
 }
